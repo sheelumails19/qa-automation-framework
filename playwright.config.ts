d4271836f80babaffa48ba8ca16372a37d80d2f2
sheelumails19 <129538404+sheelumails19@users.noreply.github.com>
2025-07-14 18:50:04 -0400

    Create playwright.config.ts

---
+++ playwright.config.ts
@@ -0,0 +1,30 @@
+// playwright.config.ts
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src/tests',
+  timeout: 30 * 1000,
+  expect: { timeout: 5000 },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: [
+    ['html'],
+    ['allure-playwright'],
+    ['json', { outputFile: 'test-results/results.json' }]
+  ],
+  use: {
+    baseURL: 'https://demo.playwright.dev',
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure'
+  },
+  projects: [
+    { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+    { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+    { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+    { name: 'Mobile Chrome', use: { ...devices['Pixel 5'] } },
+    { name: 'Mobile Safari', use: { ...devices['iPhone 12'] } }
+  ]
+});
